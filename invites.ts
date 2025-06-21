baf19fca14f821839e7f5535d37e43aa95eaf3c5
Adinette DOUGLOUI <adinettedougloui@gmil.com>
2025-06-21 13:11:36 +0100

    Initial commit

---
+++ invites.ts
@@ -0,0 +1,7 @@
+const invites = [
+  { name: 'Jean Dupont', code: '', statut: false },
+  { name: 'Marie Curie', code: '', statut: false },
+  { name: 'Albert Einstein', code: '', statut: false },
+  { name: 'Ada Lovelace', code: '', statut: false },
+  { name: 'Nelson Mandela', code: '', statut: false }
+]

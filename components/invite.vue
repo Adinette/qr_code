657ff9b6150d7b54b2968b9911294cae6eb21134
KYOGRE <azizchanou@gmail.com>
2025-06-21 13:26:55 +0100

    refactor: update component structure and fix import statement in Invite component

--- components/invite.vue
+++ components/invite.vue
@@ -1,63 +1,62 @@
-
 <script setup>
-import QcodeStream from 'vue-qrcode-reader'
+import {QrcodeStream} from 'vue-qrcode-reader'
 
-  const result = ref('')
-  const error = ref('')
+const result = ref('')
+const error = ref('')
 
-  function paintBoundingBox(detectedCodes, ctx) {
-    for (const detectedCode of detectedCodes) {
-      const {
-        boundingBox: { x, y, width, height }
-      } = detectedCode
+function paintBoundingBox(detectedCodes, ctx) {
+  for (const detectedCode of detectedCodes) {
+    const {
+      boundingBox: { x, y, width, height }
+    } = detectedCode
 
-      ctx.lineWidth = 2
-      ctx.strokeStyle = '#007bff'
-      ctx.strokeRect(x, y, width, height)
-    }
+    ctx.lineWidth = 2
+    ctx.strokeStyle = '#007bff'
+    ctx.strokeRect(x, y, width, height)
   }
+}
 
-  function onError(err) {
-    error.value = `[${err.name}]: `
+function onError(err) {
+  error.value = `[${err.name}]: `
 
-    if (err.name === 'NotAllowedError') {
-      error.value += 'you need to grant camera access permission'
-    } else if (err.name === 'NotFoundError') {
-      error.value += 'no camera on this device'
-    } else if (err.name === 'NotSupportedError') {
-      error.value += 'secure context required (HTTPS, localhost)'
-    } else if (err.name === 'NotReadableError') {
-      error.value += 'is the camera already in use?'
-    } else if (err.name === 'OverconstrainedError') {
-      error.value += 'installed cameras are not suitable'
-    } else if (err.name === 'StreamApiNotSupportedError') {
-      error.value += 'Stream API is not supported in this browser'
-    } else if (err.name === 'InsecureContextError') {
-      error.value += 'Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.'
-    } else {
-      error.value += err.message
-    }
+  if (err.name === 'NotAllowedError') {
+    error.value += 'you need to grant camera access permission'
+  } else if (err.name === 'NotFoundError') {
+    error.value += 'no camera on this device'
+  } else if (err.name === 'NotSupportedError') {
+    error.value += 'secure context required (HTTPS, localhost)'
+  } else if (err.name === 'NotReadableError') {
+    error.value += 'is the camera already in use?'
+  } else if (err.name === 'OverconstrainedError') {
+    error.value += 'installed cameras are not suitable'
+  } else if (err.name === 'StreamApiNotSupportedError') {
+    error.value += 'Stream API is not supported in this browser'
+  } else if (err.name === 'InsecureContextError') {
+    error.value += 'Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.'
+  } else {
+    error.value += err.message
   }
+}
 
-  function onDetect(detectedCodes) {
-    result.value = JSON.stringify(
-      detectedCodes.map(code => code.rawValue)
-    )
-  }
+function onDetect(detectedCodes) {
+  result.value = JSON.stringify(
+    detectedCodes.map(code => code.rawValue)
+  )
+}
 </script>
 
 <template>
-	<div>
+  <div>
     <h1>Simple Demo</h1>
 
     <p style="color: red">{{ error }}</p>
 
     <p>Last result: <b>{{ result }}</b></p>
 
-    <div style="border: 2px solid black">
+    <div style="border: 2px solid black; padding: 1rem;">
       <ClientOnly>
-        <QcodeStream :track="paintBoundingBox" @detect="onDetect" @error="onError"></QcodeStream>
+        <QrcodeStream :track="paintBoundingBox" @detect="onDetect" @error="onError"></QrcodeStream>
       </ClientOnly>
     </div>
-	</div>
+  </div>
 </template>
